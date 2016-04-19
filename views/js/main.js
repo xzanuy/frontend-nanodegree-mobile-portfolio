b9dc3a83c460b815adb04cf9bba4e28468c3e4f0
Xavier zanuy <xzanuy@gmail.com>
2016-04-19 13:08:32 +0200

    Deleting css files, Fix performance issues, add new pizza image bigger

--- views/js/main.js
+++ views/js/main.js
@@ -502,9 +502,13 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
+//getElementsByClassName is more efficient
+  var items = document.getElementsByClassName('mover');
+
+  //read out of the loop to avoid FSL
+  var phase = Math.sin((document.body.scrollTop / 1250) + 5);
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
@@ -525,7 +529,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  for (var i = 0; i < 50; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
